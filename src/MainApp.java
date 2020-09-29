202a0ba6e3c5a2125b93d5f934ef9ff24209ad01
denis <densitouch@gmail.com>
2020-09-29 18:01:59 +0300

    setBitrate() modified

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,12 +1,11 @@
-import model.Info;
 import model.UrlPlayer;
 
 public class MainApp {
     public static void main(String[] args) {
         UrlPlayer now =  new UrlPlayer();
         now.setStation("chil");
-        now.setBitrate("256");
-        System.out.printf("Station: %s\nquality: %d\n",now.getStation(),now.getBitrate());
+        now.setBitrate("320");
+        System.out.printf("Station: %s\nquality: %s\n",now.getStation(),now.getBitrate());
         new Thread(now).start();
         now.getInfo();
 
